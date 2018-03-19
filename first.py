e13c485e9e2560f75ab987da7a6d244d070b08ce
phtn458 <phtn458@gmail.com>
2018-03-19 13:26:11 -0400

    first commit

---
+++ first.py
@@ -0,0 +1,24 @@
+import random
+import sys
+import os
+
+print('hello world')
+
+#Comment
+
+'''
+multi-line comment
+'''
+
+#var
+name = 'Will'
+number = 4
+
+print('number: ', number)
+print('cube: ', number * 3)
+
+qoute = "\"always\""
+
+code = "code"
+
+print("%s %s" % (qoute, code))
